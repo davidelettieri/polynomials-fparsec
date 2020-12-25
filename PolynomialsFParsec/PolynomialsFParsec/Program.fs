ac61723fd00d7ac89542fbb9059d3825121a45ce
davidelettieri <davidelettieri@users.noreply.github.com>
2020-12-25 20:22:30 +0100

    clean up

--- PolynomialsFParsec/PolynomialsFParsec/Program.fs
+++ PolynomialsFParsec/PolynomialsFParsec/Program.fs
@@ -5,14 +5,14 @@ open FParsec
 
 type Expression = 
     | Add of Expression * Expression // x+y or z+1
-    | Const of double // 1 or 10.5
+    | Const of float // 1 or 10.5
     | Negative of Expression // -x
     | Pow of Expression * Expression // x^2
     | Product of Expression * Expression // x*y
     | Subtract of Expression * Expression // x-1 or x-y
     | Variable of char // x or y
 
-let rec eval e (x:Map<char,double>) = 
+let rec eval e (x:Map<char,float>) = 
     match e with
     | Add (e1,e2) -> eval e1 x + eval e2 x
     | Const c -> c
@@ -34,49 +34,44 @@ let getVariables e =
             | Variable v -> set [v]
 
     (impl e) |> Seq.toList
-
     
 let askVariableValues l = 
-    let rec impl l (m:Map<char,double>) = 
+    let rec impl l (m:Map<char,float>) = 
         match l with
         | [] -> m
         | h::t -> 
             printfn "Please enter value for %c" h
             let value = Console.ReadLine()
-            let m' = m.Add(h,double value)
+            let m' = m.Add(h,float value)
             impl t m'
 
     impl l (Map<_,_>([||]))
 
-// Avoid F#'s value restriction
-type UserState = unit
-type Parser<'t> = Parser<'t, UserState>
-
-let curry f = fun x -> fun y -> f(x,y)
-
-let pop : Parser<_> = pstring "("
-let pcp = pstring ")"
-let padd = stringReturn "+" (curry Add)
-let psubtract = stringReturn "-" (curry Subtract)
-let pproduct = stringReturn "*" (curry Product)
-let ppow = stringReturn "^" (curry Pow)
-
-let expression, expressionRef = createParserForwardedToRef<Expression,unit>()
-
 let pvariable = letter |>> Variable
 let pconst = pfloat |>> Const
+let expression, expressionRef = createParserForwardedToRef()
+
+let pop = skipChar '('
+let pcp = skipChar ')'
 let pprimary = choice [ pvariable; pconst; between pop pcp expression ]
-let punary = choice [ psubtract >>. pprimary |>> Negative ; pprimary]
-let pexpr = chainr1 punary ppow
-let pmultiplication = chainl1 pexpr pproduct
-let paddition = chainl1 pmultiplication (psubtract <|> padd) 
+let punary = choice [ skipChar '-' >>. pprimary |>> Negative ; pprimary]
+
+let curry f = fun x -> fun y -> f(x,y)
+let powerOp = skipChar '^' >>% (curry Pow)
+let pexpr = chainr1 punary powerOp
+
+let productOp = skipChar '*' >>% (curry Product)
+let pmultiplication = chainl1 pexpr productOp
+
+let addOp = skipChar '+' >>% (curry Add)
+let subtractOp = skipChar '-' >>% (curry Subtract)
+let paddition = chainl1 pmultiplication (subtractOp <|> addOp) 
 
 expressionRef:= paddition
 
-
 [<EntryPoint>]
 let main argv =
-    printfn "Please enter a polynomial expression"
+    printfn "Enter a polynomial expression"
     let source = Console.ReadLine()
     let polinomial = run expression source
     match polinomial with
